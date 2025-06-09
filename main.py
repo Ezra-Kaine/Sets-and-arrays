2d8bb900efc0b3df37014b364983dbed97ecb66e
Ezra-Kaine <micailaward@gmail.com>
2025-06-09 18:16:50 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#set with integers
+my_set={1,4,2,3,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5}
+print(my_set)
+#Mixed set
+mixed_set={3.141,67,"Hello", (6,7), "Yes"}
+print(mixed_set)
+#mixed_set.pop()
+print(mixed_set)
+mixed_set.remove("Yes")
+print(mixed_set)
